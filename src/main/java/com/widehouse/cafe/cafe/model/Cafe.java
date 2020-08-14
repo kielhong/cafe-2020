b976c4fd132d791dad0ebe4cc91ed974bb618c44
kielhong <kielhong@naver.com>
2020-08-15 00:06:28 +0900

    #1 resolved

---
+++ src/main/java/com/widehouse/cafe/cafe/model/Cafe.java
@@ -0,0 +1,32 @@
+package com.widehouse.cafe.cafe.model;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.ZonedDateTime;
+
+@Getter
+public class Cafe {
+    private String name;
+    private String nickname;
+    private String description;
+    private ZonedDateTime createdAt;
+    @Getter(AccessLevel.PRIVATE)
+    private boolean visible;
+
+    @Builder
+    private Cafe(String name, String nickname, String description, ZonedDateTime createdAt, boolean visible) {
+        this.name = name;
+        this.nickname = nickname;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.visible = visible;
+    }
+
+
+    public boolean isPublic() {
+        return isVisible();
+    }
+
+}
---
+++ src/test/java/com/widehouse/cafe/cafe/model/CafeTest.java
@@ -0,0 +1,36 @@
+package com.widehouse.cafe.cafe.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+class CafeTest {
+    @Test
+    void given_cafe_when_construct_then_valid() {
+        // when
+        var now = ZonedDateTime.now();
+        var cafe = Cafe.builder()
+                .name("name").nickname("nickname").description("desc").createdAt(now)
+                .build();
+        // then
+        then(cafe).satisfies(c -> {
+            then(c.getName()).isEqualTo("name");
+            then(c.getNickname()).isEqualTo("nickname");
+            then(c.getDescription()).isEqualTo("desc");
+            then(c.getCreatedAt()).isEqualTo(now);
+        });
+    }
+
+    @Test
+    void given_cafe_when_build_public_then_isPublic() {
+        // when
+        var cafe = Cafe.builder()
+                .name("name").nickname("nickname").description("desc").createdAt(ZonedDateTime.now())
+                .visible(true)
+                .build();
+        // then
+        then(cafe.isPublic()).isTrue();
+    }
+}
